a5240aa804cbd113144ee08bf36c6d2175b49daf
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-02-14 17:34:50 +0100

    Update library wallpaper

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
+const { execSync } = require('child_process')
+const wallpaper = require('wallpaper')
 
-execSync('wget https://lalanuno.files.wordpress.com/2015/02/5ccaa-david_hasselhoff_dog.jpg -O ~/cristiano.jpg')
-execSync('osascript -e \'tell application "System Events" to set picture of every desktop to ("~/cristiano.jpg")\'')
+execSync(
+  'wget https://lalanuno.files.wordpress.com/2015/02/5ccaa-david_hasselhoff_dog.jpg -O ./cristiano.jpg'
+)
+
+wallpaper.set('./cristiano.jpg').then(() => {
+  console.log('Done')
+})
